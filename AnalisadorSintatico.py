6de7aca6a3814c578f0ad16db981a32d60ff3729
Pedro Mendes <59147319+Pedrohenrimp@users.noreply.github.com>
2022-05-16 12:52:04 -0300

    Atualiza analisador para ignorar comentários

--- AnalisadorSintatico.py
+++ AnalisadorSintatico.py
@@ -9,15 +9,13 @@ formal : ID ":" TYPE
 ?expr : expr
      | expr ["@"TYPE] "." ID "(" [expr ["," expr]*] ")"
      | ID "(" [expr ["," expr]*] ")"
-     | "if" expr "then" expr ["else"]* expr ["fi"]
+     | "if" expr "then" expr "else" expr "fi"
      | "while" expr "loop" expr ["pool"]
      | "{" [expr ";"]+ "}"
      | "let" ID ":" TYPE ["<-" expr] ["," ID ":" TYPE ["<-" expr]]* "in" expr
      | "case" expr "of" [ID ":" TYPE "=>" expr ";"]+ ["esac"]
      | "new" TYPE
-     | "(" "new" TYPE ")"
      | "isvoid" expr
-     | "(" "isvoid" expr ")"
      | expr "+" expr
      | expr "-" expr
      | expr "*" expr
@@ -27,7 +25,8 @@ formal : ID ":" TYPE
      | expr "<=" expr
      | expr "=" expr
      | "not" expr
-     | (expr)
+     | COMMENT
+     | "(" expr ")"
      | ID -> id
      | "integer"
      | "string"
@@ -39,10 +38,14 @@ formal : ID ":" TYPE
 
 ID : /[a-zA-Z_][a-zA-Z_0-9]*/
 TYPE : /[A-Z_]+[a-zA-Z_0-9]*/
-STRING : /"( |[!-[]|[\]-\u10ffff]|\\(["\\\/bfnrt]|u[0-9a-zA-Z]{4}))*"/
+STRING : /"(| |[!-[]|[\]-\u10ffff]|\\([\\\/bfnrt]|u[0-9a-zA-Z]{4}))[^"]*"/
 INTEGER : /[0-9]+/
+COMMENT : /(\(\*)(\n)*(\s)*( |[!-[]|[\]-\u10ffff]|\\(["\\\/bfnrt]|u[0-9a-zA-Z]{4})|(\n)*)*(\n)*(\s)*(\*\))/ 
+        | "--"/( |[!-[]|[\]-\u10ffff]|\\(["\\\/bfnrt]|u[0-9a-zA-Z]{4}))*/ 
 
-%ignore " "+ | "\n"+
+%ignore " "+ | "\n"+ 
+             | "--"/( |[!-[]|[\]-\u10ffff]|\\(["\\\/bfnrt]|u[0-9a-zA-Z]{4}))*/ 
+             | COMMENT
 """)
 
 exemple1 = '''class Main inherits IO { main(): Object {
@@ -62,15 +65,88 @@ exemple1 = '''class Main inherits IO { main(): Object {
 exemple2 = '''class Main inherits IO {
  main() : SELF_TYPE {
  {
- out_string((new Object).type_name().substr(4,1)).
+ out_string((new Object2).type_name().substr(4,1)).
  out_string((isvoid self).type_name().substr(1,3));
  out_string("\\n");
  }
  };
 };'''
 
+exemple3 = '''class Main inherits IO {
+ main() : SELF_TYPE {
+    (let c : Complex <- (new Complex).init(1, 1) in
+    {
+    if c.reflect_X() = c.reflect_0()
+    -- teste
+    then out_string("passed\\n")
+    else out_string("failed\\n")
+    fi;
+    if c.reflect_X().reflect_Y().equal(c.reflect_0())
+    then out_string("passed\\n")
+    else out_string("failed\\n")
+    fi;
+    }
+    )
+    };
+};
+
+class Complex inherits IO {
+    x : Int;
+    y : Int;
+    init(a : Int, b : Int) : Complex {
+        {
+            x <- a;
+            y <- b;
+            self;
+            }
+            };
+            print() : Object {
+                if y = 0
+                then out_int(x)
+                else out_int(x).out_string("+").out_int(y).out_string("I")
+                fi
+                };
+                reflect_0() : Complex {
+                    {
+                        x <- ~x;
+                        y <- ~y;
+                        self;
+                        }
+                        };
+                        reflect_X() : Complex {
+                            {
+                                y <- ~y;
+                                self;
+                                }
+                                };
+                                reflect_Y() : Complex {
+                                    {
+                                        x <- ~x;
+                                        self;
+                                        }
+                                        };
+                                        equal(d : Complex) : Bool {
+                                            if x = d.x_value()
+                                            then
+                                            if y = d.y_value()
+                                            then true
+                                            else false
+                                            fi
+                                            else false
+                                            fi
+                                            };
+                                            x_value() : Int {
+                                                x
+                                                };
+                                                y_value() : Int {
+                                                    y
+                                                    };
+                                                    };'''
+
 tree1 = grammar.parse(exemple1)
 tree2 = grammar.parse(exemple2)
+tree3 = grammar.parse(exemple3)
 
-print(tree1.pretty())
-print(tree2.pretty())
+#print(tree1.pretty())
+#print(tree2.pretty())
+print(tree3.pretty())
